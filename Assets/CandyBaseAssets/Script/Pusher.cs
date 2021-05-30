6c2c5e3c9189a616ed2de9f46181105809fcc18a
Akira-Ito0109 <syobonnito@gmail.com>
2021-05-30 14:34:53 +0900

    CandyDozer 完成

---
+++ Assets/CandyBaseAssets/Script/Pusher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    Vector3 startPosition;
+
+    public float amplitude;
+    public float speed;
+
+    private void Start()
+    {
+        startPosition = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        float z = amplitude * Mathf.Sin(Time.time + speed);
+       // Debug.Log(z);
+        transform.localPosition = startPosition + new Vector3(0, 0, z);
+    }
+}
